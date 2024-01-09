2900a6569b354d68083a1861b2f44d617216361a
albertomandirola <albymandi00@gmail.com>
2024-01-09 09:25:15 +0100

    create AppHeader list

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from "./components/AppHeader.vue"
+import Main from "./components/AppMain.vue"
+import Footer from "./components/AppFooter.vue"
+export default {
+  components: {
+
+  }
+}
+</script>
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+<style lang="">
+  
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  data() {
+    return {
+      liLinks: [
+        {
+          linkName: 'Characters',
+          url: '#'
+        },
+        {
+          linkName: 'Comics',
+          url: '#'
+        },
+        {
+          linkName: 'Movies',
+          url: '#'
+        },
+        {
+          linkName: 'Tv',
+          url: '#'
+        },
+        {
+          linkName: 'Games',
+          url: '#'
+        },
+        {
+          linkName: 'Collectibles',
+          url: '#'
+        },
+        {
+          linkName: 'videos',
+          url: '#'
+        },
+        {
+          linkName: 'fans',
+          url: '#'
+        },
+        {
+          linkName: 'news',
+          url: '#'
+        },
+        {
+          linkName: 'shop',
+          url: '#'
+        },
+      ]
+    }
+  }
+}
+</script>
+<template lang="">
+  <div>
+    <img src="../assets/img/dc-logo.png" alt="logo dc comics">
+
+  </div>
+</template>
+<style lang="">
+  
+</style>
